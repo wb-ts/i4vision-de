f7d65f5b5ce02f899d052cbd0d831f19959419cc
senDev001 <sergeilipovsky@gmail.com>
2020-01-13 12:01:00 +0300

    1 on 01 13 2020

---
+++ hack.sh
@@ -0,0 +1,31 @@
+for Y in {2020..2020}
+do
+  mkdir $Y
+  cd $Y
+  for M in {01..01}
+  do
+    mkdir $M
+    cd $M
+    for D in {13..17}
+    do
+      mkdir $D
+      cd $D
+      for i in $(seq 1 $((1 + RANDOM % 5))) 
+      do
+        echo "$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+      done
+      cd ../
+    done
+    cd ../
+  done
+  cd ../
+done
+git push origin master
+git rm -rf 20**
+git rm -rf 19**
+git commit -am "cleanup"
+git push origin master
